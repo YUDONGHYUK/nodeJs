6212d4a5fc0a5b3a4f12e8e8dd51940bdc7c5b1f
doyu <ydh0905@nate.com>
2023-02-21 15:51:03 +0900

    built-in-modules: fs promise modules

--- built-in-modules/fs/index.js
+++ built-in-modules/fs/index.js
@@ -1,3 +1,35 @@
+// async-await
+
+const fs = require('node:fs/promises');
+
+async function readFile() {
+  try {
+    const data = await fs.readFile('./file.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+readFile();
+
+// ----------------------------------------------
+/* fs Promise Modules
+
+const fs = require('node:fs/promises');
+
+console.log('First');
+
+fs.readFile('./file.txt', 'utf-8')
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+console.log('Second');
+*/
+
+// ----------------------------------------------
+/* fs Module
+
 const fs = require('node:fs');
 
 console.log('First');
@@ -26,3 +58,5 @@ fs.writeFile('./greet.txt', ' Hello Doyu!', { flag: 'a' }, (err) => {
     console.log('File written');
   }
 });
+
+*/
