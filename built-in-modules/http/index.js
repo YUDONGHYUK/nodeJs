83f8bd363bf742d0968800f61286905849aa3df7
doyu <ydh0905@nate.com>
2023-02-28 12:59:52 +0900

    built-in-modules: HTTP response

--- built-in-modules/http/index.js
+++ built-in-modules/http/index.js
@@ -1,13 +1,22 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-  const superHero = {
-    firstName: 'Bruce',
-    lastName: 'Wayne',
-  };
+  // const superHero = {
+  //   firstName: 'Bruce',
+  //   lastName: 'Wayne',
+  // };
 
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(superHero));
+  // readFileSync는 파일의 전체 내용을 한 번에 읽기 때문에
+  // 큰 HTML 파일이 있으면 모든 콘텐츠를 임시 Buffer에 저장하므로 불필요한 메모리 사용으로 이어질 수 있다.
+  // 대신 stream을 사용할 수 있다.
+  // const html = fs.readFileSync('./index.html', 'utf-8');
+  // res.end(html);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
+
+  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // res.end(JSON.stringify(superHero));
 
   // res.writeHead(200, { 'Content-Type': 'text/plain' });
   // res.end('Hello world!');
