b5188565624feb122c65eb0a3a51f7a2d876a789
doyu <ydh0905@nate.com>
2023-03-02 21:09:01 +0900

    built-in-modules: HTML template

--- built-in-modules/http/index.js
+++ built-in-modules/http/index.js
@@ -7,13 +7,22 @@ const server = http.createServer((req, res) => {
   //   lastName: 'Wayne',
   // };
 
-  // readFileSync는 파일의 전체 내용을 한 번에 읽기 때문에
-  // 큰 HTML 파일이 있으면 모든 콘텐츠를 임시 Buffer에 저장하므로 불필요한 메모리 사용으로 이어질 수 있다.
-  // 대신 stream을 사용할 수 있다.
-  // const html = fs.readFileSync('./index.html', 'utf-8');
-  // res.end(html);
+  const name = 'Doyu';
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  let html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+  html = html.replace('{{name}}', name);
+  res.end(html);
+
+  /* HTML response
+  readFileSync는 파일의 전체 내용을 한 번에 읽기 때문에
+  큰 HTML 파일이 있으면 모든 콘텐츠를 임시 Buffer에 저장하므로 불필요한 메모리 사용으로 이어질 수 있다.
+  대신 stream을 사용할 수 있다.
+
+  const html = fs.readFileSync('./index.html', 'utf-8');
+  res.end(html);
   res.writeHead(200, { 'Content-Type': 'text/html' });
   fs.createReadStream(__dirname + '/index.html').pipe(res);
+  */
 
   // res.writeHead(200, { 'Content-Type': 'application/json' });
   // res.end(JSON.stringify(superHero));
