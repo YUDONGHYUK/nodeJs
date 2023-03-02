1cd5110b865589f11fa344625237db4014fc612c
doyu <ydh0905@nate.com>
2023-03-02 22:09:50 +0900

    built-in-modules: HTTP routing

--- built-in-modules/http/index.js
+++ built-in-modules/http/index.js
@@ -2,16 +2,34 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
+  // res.end(req.url);
+
+  if (req.url === '/') {
+    res.writeHead('200', { 'Content-Type': 'text/plain' });
+    res.end('Home page');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('About page');
+  } else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ firstName: 'Bruce', lastName: 'Wayne' }));
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
+
   // const superHero = {
   //   firstName: 'Bruce',
   //   lastName: 'Wayne',
   // };
 
+  /* HTTP template
   const name = 'Doyu';
   res.writeHead(200, { 'Content-Type': 'text/html' });
   let html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
   html = html.replace('{{name}}', name);
   res.end(html);
+  */
 
   /* HTML response
   readFileSync는 파일의 전체 내용을 한 번에 읽기 때문에
