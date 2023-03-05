c5c8e78e0230b74a52eb2455fcbd5d6d3c4b6b25
doyu <ydh0905@nate.com>
2023-03-05 23:20:55 +0900

    libuv: Thread Pool

---
+++ libuv/thread-pool/index.js
@@ -0,0 +1,17 @@
+const crypto = require('node:crypto');
+
+const MAX_CALLS = 2;
+
+const start = Date.now();
+for (let i = 0; i < MAX_CALLS; i++) {
+  crypto.pbkdf2('password', 'salt', 100000, 512, 'sha512', () => {
+    console.log(`Hash: ${i + 1}`, Date.now() - start);
+  });
+}
+
+/* Synchronous Version
+const start = Date.now();
+crypto.pbkdf2Sync('password', 'salt', 100000, 512, 'sha512');
+crypto.pbkdf2Sync('password', 'salt', 100000, 512, 'sha512');
+console.log('Hash: ', Date.now() - start);
+*/
