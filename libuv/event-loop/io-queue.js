5d132d119b7e277d449a3f07d2d3cd3e31f4a9c9
doyu <ydh0905@nate.com>
2023-03-19 15:58:25 +0900

    libuv: I/O queue

---
+++ libuv/event-loop/io-queue.js
@@ -0,0 +1,41 @@
+// microtask Queue + timer Queue + I/O Queue
+
+const fs = require('node:fs');
+
+fs.readFile(__filename, () => {
+  console.log('this is readFile 1');
+});
+
+process.nextTick(() => console.log('this is process.nextTick 1'));
+Promise.resolve().then(() => console.log('this is Promise.resolve 1'));
+setTimeout(() => console.log('this is setTimeout 1'), 0);
+
+for (let i = 0; i < 2000000000; i++) {}
+
+/* timer Queue + I/O Queue
+
+const fs = require('node:fs');
+
+setTimeout(() => console.log('this is setTimeout 1'));
+
+fs.readFile(__filename, () => {
+  console.log('this is readFile 1');
+});
+
+// 실행 순서 보장 안됨...
+*/
+
+/* microtask Queue + I/O queue
+
+const fs = require('node:fs');
+
+fs.readFile(__filename, () => {
+  console.log('this is readFile 1');
+});
+
+process.nextTick(() => console.log('this is process.nextTick 1'));
+Promise.resolve().then(() => console.log('this is Promise.resolve 1'));
+
+실행 순서
+nextTick Queue -> promise Queue -> I/O Queue
+*/
