5736e516b93fed8af32cc2765259b1d3e269fa77
doyu <ydh0905@nate.com>
2023-03-26 23:50:53 +0900

    npm: global packages & npm scripts

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,7 +1,8 @@
 const upperCase = require('upper-case').upperCase;
 
 function greet(name) {
-  console.log(upperCase(`Hello ${name}, welcome to node.js`));
+  // console.log(upperCase(`Hello ${name}, welcome to node.js`));
+  console.log(upperCase(`Hello ${name}, welcome to Global Packages.`));
 }
 
 greet('doyu');
