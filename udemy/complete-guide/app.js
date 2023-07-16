b9ea9c1c199a6156ba152e3f67ee62e5644b1e7e
doyu <ydh0905@nate.com>
2023-07-16 19:54:27 +0900

    udemy: Add middleware

--- udemy/complete-guide/app.js
+++ udemy/complete-guide/app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
+// use: Middleware를 실행시키기 위한 메서드
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log('In the another middleware');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
