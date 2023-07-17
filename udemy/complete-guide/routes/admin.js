b0491eaa2325131d830a624e7f552e72e10dc09b
doyu <ydh0905@nate.com>
2023-07-17 23:12:45 +0900

    udemy: Add helper for navigation

--- udemy/complete-guide/routes/admin.js
+++ udemy/complete-guide/routes/admin.js
@@ -2,11 +2,13 @@ const path = require('node:path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // route: /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
   // res.send(`<form action="/admin/product" method="POST">
   //   <input type="text" name="title" />
   //   <button type="submit">Add Product</button>
