1569307cba36082a72c2b7a3c120b28ed5d41a2a
doyu <ydh0905@nate.com>
2023-07-17 21:33:54 +0900

    udemy: Add html page(shop, add-product, 404)

--- udemy/complete-guide/routes/shop.js
+++ udemy/complete-guide/routes/shop.js
@@ -1,9 +1,12 @@
+const path = require('node:path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send(`<h1>Hello from Express!</h1>`);
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
