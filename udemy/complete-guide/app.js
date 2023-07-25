b5732cac9eb7715f77519c801278a7eab8f3eb93
doyu <ydh0905@nate.com>
2023-07-25 22:45:20 +0900

    udemy: Add controller in the MVC pattern

--- udemy/complete-guide/app.js
+++ udemy/complete-guide/app.js
@@ -2,35 +2,27 @@ const path = require('node:path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 /* use: Middleware를 실행시키기 위한 메서드
         모든 http 메서드(GET, POST 등...)에 반응
-
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continue to the next middleware in line
-});
 */
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoute);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
-// const server = http.createServer(app);
-// server.listen(3000);
--- udemy/complete-guide/routes/admin.js
+++ udemy/complete-guide/routes/admin.js
@@ -2,33 +2,13 @@ const path = require('node:path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 // route: /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCss: true,
-    productCss: true,
-    activeAddProduct: true,
-  });
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  // res.send(`<form action="/admin/product" method="POST">
-  //   <input type="text" name="title" />
-  //   <button type="submit">Add Product</button>
-  // </form>`);
-});
+router.get('/add-product', productsController.getAddProduct);
 
-// route: /admin/product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
