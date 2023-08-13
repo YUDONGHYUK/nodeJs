46e844ef5dca8b059775bd96709052f34c2edb2c
doyu <ydh0905@nate.com>
2023-08-13 20:24:53 +0900

    udemy: Add product detail page using dynamic route

--- udemy/complete-guide/models/product.js
+++ udemy/complete-guide/models/product.js
@@ -26,6 +26,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -36,5 +37,12 @@ module.exports = class Product {
 
   static fetchAll(callback) {
     getProductFromFile(callback);
+  }
+
+  static findById(id, callback) {
+    getProductFromFile((products) => {
+      const product = products.find((item) => item.id === id);
+      callback(product);
+    });
   }
 };
