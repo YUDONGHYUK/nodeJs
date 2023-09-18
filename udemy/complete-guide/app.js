447cb09f0ce102ca4f9df87c431fef6ebff48f05
doyu <ydh0905@nate.com>
2023-09-18 23:44:11 +0900

    udemy: Install mysql2, connect app to mysql and create basic sql, table

--- udemy/complete-guide/app.js
+++ udemy/complete-guide/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoute = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result[0], result[1]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 /* use: Middleware를 실행시키기 위한 메서드
         모든 http 메서드(GET, POST 등...)에 반응
