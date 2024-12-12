3fc371d6358d2165f5588bc4300e4f0a55eae26a
doyu <ydh0905@nate.com>
2024-12-12 22:54:57 +0900

    udemy: express 경로 필터링

--- udemy/complete-guide/routes/admin.js
+++ udemy/complete-guide/routes/admin.js
@@ -2,16 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+// Path: /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.send(`
-    <form action='/product' method='POST'>
+    <form action='/admin/add-product' method='POST'>
       <input type='text' name='title' />
       <button type='submit'>Add Product</button
     </form>
   `);
 });
 
-router.post('/product', (req, res, next) => {
+// Path: /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
