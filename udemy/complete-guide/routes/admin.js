bfc49b11880d5c2f18abb151c1050220fd857247
doyu <ydh0905@nate.com>
2024-12-18 23:40:37 +0900

    udemy: HTML 페이지 생성 및 rooDir 유틸 추가

--- udemy/complete-guide/routes/admin.js
+++ udemy/complete-guide/routes/admin.js
@@ -1,15 +1,12 @@
+const path = require('path');
 const express = require('express');
+const { rootDir } = require('../utils/path');
 
 const router = express.Router();
 
 // Path: /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.send(`
-    <form action='/admin/add-product' method='POST'>
-      <input type='text' name='title' />
-      <button type='submit'>Add Product</button
-    </form>
-  `);
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // Path: /admin/add-product => POST
