56a400054c97ed9365914f6f85aca8bb6b6604ee
doyu <ydh0905@nate.com>
2024-12-22 22:51:02 +0900

    udemy: 스타일 추가 및 정적(static) 파일 서비스 추가

--- udemy/complete-guide/app.js
+++ udemy/complete-guide/app.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public'))); // 정적 파일 서비스
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
