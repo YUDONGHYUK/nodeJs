6a0eb045a534aa0d748751fd5f78b05be544a1b6
doyu <ydh0905@nate.com>
2024-12-30 23:14:36 +0900

    udemy: controller 추가

---
+++ udemy/complete-guide/controllers/products.js
@@ -0,0 +1,28 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+   // render : 템플릿 엔진을 사용할 때 사용, 등록된 view engine을 찾아 실행한다.
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  })
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+}
--- udemy/complete-guide/routes/admin.js
+++ udemy/complete-guide/routes/admin.js
@@ -1,29 +1,16 @@
 const path = require('path');
 const express = require('express');
-const { rootDir } = require('../utils/path');
+
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
+
 
 // Path: /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  // render : 템플릿 엔진을 사용할 때 사용, 등록된 view engine을 찾아 실행한다.
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // Path: /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
