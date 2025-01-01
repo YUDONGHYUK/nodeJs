73615a0492eff7f056b709e15624edd10fa966b5
doyu <ydh0905@nate.com>
2025-01-01 14:23:30 +0900

    udemy: product model 추가

--- udemy/complete-guide/controllers/products.js
+++ udemy/complete-guide/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
    // render : 템플릿 엔진을 사용할 때 사용, 등록된 view engine을 찾아 실행한다.
@@ -12,17 +12,21 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
+  Product.fetchAll( (products) => {
+      res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+      })
+    } 
+  )
 }
