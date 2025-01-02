8b37eb5f303ba39299cda289dff50992c5bd6cc1
doyu <ydh0905@nate.com>
2025-01-02 23:34:46 +0900

    udemy: 동적 라우트 추가

--- udemy/complete-guide/controllers/shop.js
+++ udemy/complete-guide/controllers/shop.js
@@ -1,44 +1,53 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products',
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
+    });
+  });
+};
 
-      })
-    } 
-  )
-}
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: `/products`,
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/index', {
       prods: products,
-    pageTitle: 'Shop',
+      pageTitle: 'Shop',
       path: '/',
-    })
-  })
-}
+    });
+  });
+};
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     pageTitle: 'Your Cart',
-    path: '/cart'
-  })
-}
+    path: '/cart',
+  });
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     pageTitle: 'Orders',
-    path: '/orders'
-  })
-}
+    path: '/orders',
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
-    pageTitle: 'Checkout'
-  })
-}
+    pageTitle: 'Checkout',
+  });
+};
